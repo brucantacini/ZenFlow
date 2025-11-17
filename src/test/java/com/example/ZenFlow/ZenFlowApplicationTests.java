bd8d9d2768b997040af65a03afbbccbd35ab37c7
Bruno Cantacini <brunocantacini.ti@gmail.com>
2025-11-17 14:44:20 -0300

    feat: subindo projeto

---
+++ src/test/java/com/example/ZenFlow/ZenFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZenFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
